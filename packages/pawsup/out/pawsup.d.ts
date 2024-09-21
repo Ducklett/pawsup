512b77e9ad1345fcb9edcd472ea8cf5ebc331086
ivy <ivy16203@gmail.com>
2024-09-21 17:22:04 +0200

    build script, add types to out dir

---
+++ packages/pawsup/out/pawsup.d.ts
@@ -0,0 +1,4 @@
+declare function pawsupInline(input: string): string;
+declare function pawsup(input: string): string;
+export declare function paw(strings: TemplateStringsArray, ...values: any[]): string;
+export { pawsup, pawsupInline };
